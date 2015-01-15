81125aebd8c9de8d2ccfcbcd2abc3a2d9d37aabb
andypaxo <andy@andypaxo.net>
2015-01-14 18:46:53 -0700

    Brand new LibGDX project

---
+++ desktop/src/net/softwarealchemist/adrift/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.softwarealchemist.adrift.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.softwarealchemist.adrift.AdriftMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AdriftMain(), config);
+	}
+}
