9df23a040ee3c75c656f4702adf84cc7551b6326
Andy <andy@andypaxo.net>
2015-03-05 17:35:29 -0700

    Some usability tweaks

--- core/src/net/softwarealchemist/adrift/Sounds.java
+++ core/src/net/softwarealchemist/adrift/Sounds.java
@@ -10,11 +10,13 @@ public class Sounds {
 	private static Sound bling;
 	private static Music loop;
 	private static Entity player;
+	private static float loopVolume = 0;
 	
 	public static void startup(Entity playerCharacter) {
 		bling = Gdx.audio.newSound(Gdx.files.internal("sounds/bling.wav"));
 		loop = Gdx.audio.newMusic(Gdx.files.internal("sounds/relic-loop.mp3"));
 		loop.setLooping(true);
+		loop.setVolume(loopVolume);
 		loop.play();
 		player = playerCharacter; 
 	}
@@ -25,7 +27,9 @@ public class Sounds {
 	}
 	
 	public static void setLoopDistance(float distance) {
-		loop.setVolume(1f / distance);
+		float targetVolume = distance < 32 ? 1f / distance : 0;
+		loopVolume = (targetVolume + loopVolume * 19f) / 20f;
+		loop.setVolume(loopVolume);
 	}
 	
 	public static void shutdown() {
--- desktop/src/net/softwarealchemist/adrift/desktop/DesktopLauncher.java
+++ desktop/src/net/softwarealchemist/adrift/desktop/DesktopLauncher.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class DesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 600;
 		config.title = "adrift";
 		new LwjglApplication(new AdriftGame(), config);
 	}
