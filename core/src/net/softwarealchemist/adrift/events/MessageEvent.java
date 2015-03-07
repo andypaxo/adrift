34cb50d27e30f75ceec72fb1f8e41d03ed731849
Andy <andy@andypaxo.net>
2015-03-06 19:14:01 -0700

    Message event

---
+++ core/src/net/softwarealchemist/adrift/events/MessageEvent.java
@@ -0,0 +1,35 @@
+package net.softwarealchemist.adrift.events;
+
+import java.io.Serializable;
+import java.util.List;
+
+import net.softwarealchemist.adrift.Hud;
+import net.softwarealchemist.adrift.entities.Entity;
+import net.softwarealchemist.network.ClientListener;
+
+public class MessageEvent implements Event, Serializable {
+
+	private static final long serialVersionUID = -6701891783690653614L;
+
+	private int playerId;
+	private String message;
+
+	public MessageEvent(int playerId, String message) {
+		this.playerId = playerId;
+		this.message = message;
+	}
+
+	@Override
+	public void execute(ClientListener listener) {
+		Entity player = listener.getEntityById(playerId);
+		String playerName = player == listener.getPlayer() ? "Me" : player.name;
+		Hud.log(playerName + " : " + message);
+	}
+
+	@Override
+	public List<Event> executeServer(ClientListener listener) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
